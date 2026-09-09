c2ca17f722e6bcc6e979bc903569efec46fa27a5
michael.noiret <michael.noiret@laplateforme.io>
2026-09-09 15:23:14 +0200

    Ajout Jour05 - Job03

---
+++ Jour05/Job03/Joueur.hpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Joueur
+{
+private :
+    int x;
+    int y;
+
+public :
+
+    Joueur(int x, int y)
+    {
+        x = x;
+        y = y;
+    }
+
+    Joueur(int x, int y)
+    {
+        x = 0;
+        y = 0;
+    }
+
+    // Joueur(int x, int y)
+    // {
+    //     x = 0;
+    //     y = 0;
+    // }
+
+    Joueur(int x, int y, string nom)
+    {
+        x = 0;
+        y = 0;
+        nom = "";
+    }
+    
+    int setx(int x)
+    {
+        cin >> x;
+        return x;
+    }
+
+    int getx()
+    {
+        return x;
+    }
+
+    int sety(int y)
+    {
+        cin >> y;
+        return y;
+    }
+
+    int gety()
+    {
+        return y;
+    }
+
+    void afficher(int x,int y)
+    {
+        cout << "x vaut : " << x << endl;
+        cout << "y vaut : " << y << endl;
+    }
+
+    // int deplacer(int x, int y)
+    // {
+    //     setx(x++);
+    //     sety(y++);
+    // }
+
+int main()
+{
+
+    return 0;
+}
+};
+
---
+++ Jour05/Job03/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include "Joueur.hpp"
+using namespace std;
+
+int main()
+{
+    Joueur j1(0, 0);
+    j1.afficher(0, 0);
+    cout << j1.getx() << endl;
+    // j1.setx(+1);
+    // cout << j1.gety() << endl;
+    // j1.sety(+1);
+    // j1.deplacer(x, y);
+    // j1.afficher(1, 1);
+    Joueur j2(0,0);
+    j2.afficher(0,0);
+    cout << j1.getx() << endl;
+    // j2.setx(+1);
+    // cout << j2.gety() << endl;
+    // j2.sety(+1);
+    // j2.deplacer(x, y);
+    // j2.afficher(1, 1);
+    Joueur j3(0,0);
+    j3.afficher(0,0);
+    // j3.setx(+1);
+    // cout << j3.gety() << endl;
+    // j3.sety(+1);
+    // j3.deplacer(x, y);
+    // j3.afficher(1, 1);
+    Joueur j4(0,0);
+    j4.afficher(0,0);
+    // j4.setx(+1);
+    // cout << j4.gety() << endl;
+    // j4.sety(+1);
+    // j4.deplacer(x, y);
+    // j4.afficher(1, 1);
+
+return 0;
+}
